fc3a743070ccda5a89856cf8da5c981f1c346d42
ChristianNikodemus <christian.nikodemus@gmail.com>
2021-08-28 21:05:43 -0400

    Used morgan middle ware JS to log all requests

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-const express = require('express')
+const express = require('express'),
+  morgan = require('morgan');
 
 const app = express();
 
 app.use(express.static('public'));
+
+app.use(morgan('common'));
 
 // JSON object containing my top 10 movies
 let topMovies = [
