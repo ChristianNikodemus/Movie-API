c51dc901fed9c0f68b3b831c1c3f9e0cba914044
ChristianNikodemus <christian.nikodemus@gmail.com>
2021-08-29 22:23:37 -0400

    Added more information to each movie in my JSON array

--- index.js
+++ index.js
@@ -19,71 +19,71 @@ let topMovies = [
         director: 'Quentin Tarantino',
         year: '1994',
         description: 'The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption.',
-        genre: 'Crime/Drama',
+        genre: 'Crime/ Drama',
         //image: img.src = '/public/img/pulpfiction.png'
     },
     {
         title: 'The Lord of the Rings: The Return of the King',
         director: 'Peter Jackson',
         year: '2003',
-        description: '',
-        genre: ''
+        description: 'Gandalf and Aragorn lead the World of Men against Sauron\'s army to draw his gaze from Frodo and Sam as they approach Mount Doom with the One Ring.',
+        genre: 'Action/ Adventure/ Drama'
     },
     {
         title: 'Forest Gump',
         director: 'Robert Zemeckis',
         year: '1994',
-        description: '',
-        genre: ''
+        description: 'The presidencies of Kennedy and Johnson, the Vietnam War, the Watergate scandal and other historical events unfold from the perspective of an Alabama man with an IQ of 75, whose only desire is to be reunited with his childhood sweetheart.',
+        genre: 'Drama/ Romance'
     },
     {
         title: 'Back to the Future',
         director: 'Robert Zemeckis',
         year: '1985',
-        description: '',
-        genre: ''
+        description: 'Marty McFly, a 17-year-old high school student, is accidentally sent thirty years into the past in a time-traveling DeLorean invented by his close friend, the eccentric scientist Doc Brown.',
+        genre: 'Adventure/ Comedy/ Sci-Fi'
     },
     {
         title: 'The Lion King',
         director: 'Roger Allers & Rob Minkoff',
         year: '1960',
-        description: '',
-        genre: ''
+        description: 'Lion prince Simba and his father are targeted by his bitter uncle, who wants to ascend the throne himself.',
+        genre: 'Animation/ Adventure/ Drama'
     },
     {
         title: 'Psycho',
         director: 'Alfred Hitchcock',
         year: '1960',
-        description: '',
-        genre: ''
+        description: 'A Phoenix secretary embezzles $40,000 from her employer\'s client, goes on the run, and checks into a remote motel run by a young man under the domination of his mother.',
+        genre: 'Horror/ Mystery/ Thriller'
     },
     {
         title: 'Django Unchained',
         director: 'Quentin Tarantino',
         year: '2012',
-        description: '',
-        genre: ''
+        description: 'With the help of a German bounty-hunter, a freed slave sets out to rescue his wife from a brutal plantation-owner in Mississippi.',
+        genre: 'Drama/ Western'
     },
     {
         title: '2001: A Space Odyssey',
         director: 'Stanley Kubrick',
         year: '1968',
-        description: '',
-        genre: ''
+        description: 'After discovering a mysterious artifact buried beneath the Lunar surface, mankind sets off on a quest to find its origins with help from intelligent supercomputer H.A.L. 9000.',
+        genre: 'Adventure/ Sci-Fi'
     },
     {
         title: 'Finding Nemo',
         director: 'Andrew Stanton',
         year: '2003',
-        description: '',
-        genre: ''
+        description: 'After his son is captured in the Great Barrier Reef and taken to Sydney, a timid clownfish sets out on a journey to bring him home.',
+        genre: 'Animation/ Adventure/ Comedy'
     },
     {
         title: 'No Country for Old Men',
         director: 'Ethan Coen & Joel Coen',
         year: '2007',
-        description: '',
-        genre: ''
+        description: 'Violence and mayhem ensue after a hunter stumbles upon a drug deal gone wrong and more than two million dollars in cash near the Rio Grande.',
+        genre: 'Crime/ Drama/ Thriller'
     }
 ];
 
