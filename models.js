0ecb23f95f886e0624de90f620143a4889cbed5b
ChristianNikodemus <christian.nikodemus@gmail.com>
2021-09-21 22:27:36 -0400

    Put Model.js into prettier.js

--- models.js
+++ models.js
@@ -1,50 +1,50 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 let movieSchema = mongoose.Schema({
-  Title: {type: String, required: true},
-  Description: {type: String, required: true},
+  Title: { type: String, required: true },
+  Description: { type: String, required: true },
   Year: String,
-  Genre: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Genre' }],
-  Director: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Director' }],
+  Genre: [{ type: mongoose.Schema.Types.ObjectId, ref: "Genre" }],
+  Director: [{ type: mongoose.Schema.Types.ObjectId, ref: "Director" }],
   //Actors: [String],
   ImagePath: String,
-  Featured: Boolean
+  Featured: Boolean,
 });
 
 let userSchema = mongoose.Schema({
-    Name: {type: String},
-    Username: {type: String, required: true},
-    Email: {type: String, required: true},
-    Password: {type: String, required: true},
-    Birthday: Date,
-    favouriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
+  Name: { type: String },
+  Username: { type: String, required: true },
+  Email: { type: String, required: true },
+  Password: { type: String, required: true },
+  Birthday: Date,
+  favouriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: "Movie" }],
 });
 
 userSchema.statics.hashPassword = (password) => {
   return bcrypt.hashSync(password, 10);
 };
 
-userSchema.methods.validatePassword = function(password) {
+userSchema.methods.validatePassword = function (password) {
   return bcrypt.compareSync(password, this.Password);
 };
 
 let genreSchema = mongoose.Schema({
-  Title: {type: String, required: true},
-  Description: {type: String, required: true},
+  Title: { type: String, required: true },
+  Description: { type: String, required: true },
 });
 
 let directorSchema = mongoose.Schema({
-  Name: {type: String, required: true},
-  Bio: {type: String, required: true},
-  Birthyear: {type: String, required: true},
-  Deathyear: String
+  Name: { type: String, required: true },
+  Bio: { type: String, required: true },
+  Birthyear: { type: String, required: true },
+  Deathyear: String,
 });
 
-let Movie = mongoose.model('Movie', movieSchema);
-let User = mongoose.model('User', userSchema);
-let Genre = mongoose.model('Genre', genreSchema);
-let Director = mongoose.model('Director', directorSchema);
+let Movie = mongoose.model("Movie", movieSchema);
+let User = mongoose.model("User", userSchema);
+let Genre = mongoose.model("Genre", genreSchema);
+let Director = mongoose.model("Director", directorSchema);
 
 module.exports.Movie = Movie;
 module.exports.User = User;
