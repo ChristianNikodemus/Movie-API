e993dbd7c23a4597ed8d2790d514fb7dae1ee8e0
ChristianNikodemus <christian.nikodemus@gmail.com>
2021-09-29 18:41:54 -0400

    Used different type of function instead of cors to avoid Chrome browser error

--- index.js
+++ index.js
@@ -26,8 +26,21 @@ app.use(bodyParser.json());
 app.use(morgan("common"));
 
 // Cross-Origin Resource Sharing (CORS)
-const cors = require("cors");
-app.use(cors());
+//const cors = require("cors");
+//app.use(cors());
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', req.get('Origin') || '*');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+  res.header('Access-Control-Expose-Headers', 'Content-Length');
+  res.header('Access-Control-Allow-Headers', 'Accept, Authorization, Content-Type, X-Requested-With, Range');
+  if (req.method === 'OPTIONS') {
+    return res.send(200);
+  } else {
+    return next();
+  }
+});
 
 app.use(express.static("public"));
 
