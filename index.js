db8c5e1241f831c1e845fe3e74e32d2f695e2cd1
ChristianNikodemus <christian.nikodemus@gmail.com>
2021-09-30 15:08:28 -0400

    added quotation to cross origin for consistency

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
   res.header('Access-Control-Expose-Headers', 'Content-Length');
   res.header('Access-Control-Allow-Headers', 'Accept, Authorization, Content-Type, X-Requested-With, Range');
-  res.header('Cross-Origin-Resource-Policy', 'cross-origin')
+  res.header('Cross-Origin-Resource-Policy', 'cross-origin');
   if (req.method === 'OPTIONS') {
     return res.send(200);
   } else {
